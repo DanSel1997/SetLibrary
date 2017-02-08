08520ddfa8929c28d6a856c31fd5ee18fc6eccef
Alexander <avi2011class@yandex.ru>
2017-02-08 23:22:16 +0300

    initial commit with header and main cpp file

---
+++ header.h
@@ -0,0 +1,25 @@
+
+#include <stdint.h>
+
+#define CONTAINER_TREE	0
+#define CONTAINER_HASH	1
+
+struct Set {
+	uint8_t type_of_container;
+	size_t key_size;
+	size_t data_size;
+	
+	void* structure_pointer;
+
+	void (*set)(struct Set* S, const void* key);
+	void (*get)(struct Set* S, const void* key, void* buffer);
+	uint8_t (*find)(struct Set* S, const void* key);
+	void (*destroy)(struct Set* S);
+};
+
+struct Set* SetTreeCreate(size_t key_size, size_t data_size);
+struct Set* SetHashCreate(size_t key_size, size_t data_size);
+
+
+
+ 
---
+++ libcore.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+#include "header.h"
+
+// Prototypes of main Tree functions
+void	TreeSetValue(struct Set* S, const void* key, const void* data);
+int		TreeGetValue(struct Set* S, const void* key, void* buffer);
+uint8_t TreeFindValue(struct Set* S, const void* key);
+void*	TreeInit();
+void	TreeDestroy(struct Set* S);
+
+// Prototypes of main Hash functions
+void	HashSetValue(struct Set* S, const void* key, const void* data);
+int		HashGetValue(struct Set* S, const void* key, void* buffer);
+uint8_t HashFindValue(struct Set* S, const void* key);
+void*	HashInit();
+void	HashDestroy(struct Set* S);
+
+
+struct Set* SetTreeCreate(size_t key_size, size_t data_size)
+{
+	struct Set* result = (struct Set*) calloc (sizeof(struct Set), 1);
+
+	result->key_size = key_size;
+	result->data_size = data_size;
+	result->type_of_container = CONTAINER_TREE;
+
+	result->structure_pointer =  TreeInit();
+
+	result->set = TreeSetValue;
+	result->get = TreeGetValue;
+	result->find = TreeFindValue;
+	result->destroy = TreeDestroy;
+}
+
+struct Set* SetHashCreate(size_t key_size, size_t data_size)
+{
+	struct Set* result = (struct Set*) calloc (sizeof(struct Set), 1);
+
+	result->key_size = key_size;
+	result->data_size = data_size;
+	result->type_of_container = CONTAINER_HASH;
+
+	result->structure_pointer =  HashInit();
+
+	result->set = HashSetValue;
+	result->get = HashGetValue;
+	result->find = HashFindValue;
+	result->destroy = HashDestroy;
+}
+
+
